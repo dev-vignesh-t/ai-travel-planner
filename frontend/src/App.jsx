4b1c362884e6f64e1137ad80bbe7ef96751ae707
vignesh-t-dev <dev.vignesht@gmail.com>
2026-06-20 20:39:24 +0530

    Add complete frontend folder: Vite config, package.json, components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-indigo-600">Tailwind is working</h1>
+    </>
+      )
+}
+
+export default App
